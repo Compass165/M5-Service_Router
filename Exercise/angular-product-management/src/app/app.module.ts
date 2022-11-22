483bcded87c40f09e159e83a4d3b67423793340a
Compass <ndphong165@gmail.com>
2022-11-22 17:18:07 +0700

    [Bài tập] Hoàn thiện ứng dụng quản lý sản phẩm

--- Exercise/angular-product-management/src/app/app.module.ts
+++ Exercise/angular-product-management/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { ProductCreateComponent } from './product-create/product-create.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { ProductDeleteComponent } from './product-delete/product-delete.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductCreateComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDeleteComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
